d9985e5b33154bff058f82860ad45dd6659b7123
PJ.Javis <jiangpeng59@qq.com>
2018-12-05 09:32:19 +0800

    update main.py

--- README.md
+++ README.md
@@ -13,10 +13,14 @@
 数据和权重可以使用百度云下载 链接：https://pan.baidu.com/s/10E0CIehRMUA7zepmSbtjjA 提取码：wabv 
 
 ## 模型训练
-python main.py #在main调用train()方法
+```
+python main.py train
+```
 
 ## 测试模型训练
 加载权重，默认保存最后一个权重
-python main.py #在main调用infer()方法
+```
+python main.py test --ckp=weights_19.pth
+```
 
 ![](https://img-blog.csdn.net/20180508083935908)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import torch
+import argparse
 from torch.utils.data import DataLoader
 from torch import autograd, optim
 from torchvision.transforms import transforms
 from unet import Unet
 from dataset import LiverDataset
+
 
 # 是否使用cuda
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -17,6 +19,8 @@ x_transforms = transforms.Compose([
 # mask只需要转换为tensor
 y_transforms = transforms.ToTensor()
 
+#参数解析
+parse=argparse.ArgumentParser()
 
 def train_model(model, criterion, optimizer, dataload, num_epochs=20):
     for epoch in range(num_epochs):
@@ -45,7 +49,7 @@ def train_model(model, criterion, optimizer, dataload, num_epochs=20):
 #训练模型
 def train():
     model = Unet(3, 1).to(device)
-    batch_size = 8
+    batch_size = args.batch_size
     criterion = torch.nn.BCELoss()
     optimizer = optim.Adam(model.parameters())
     liver_dataset = LiverDataset("data/train",transform=x_transforms,target_transform=y_transforms)
@@ -53,11 +57,12 @@ def train():
     train_model(model, criterion, optimizer, dataloaders)
 
 #显示模型的输出结果
-def infer():
+def test():
     model = Unet(3, 1)
-    model.load_state_dict(torch.load('weights_19.pth',map_location='cpu'))
+    model.load_state_dict(torch.load(args.ckp,map_location='cpu'))
     liver_dataset = LiverDataset("data/val", transform=x_transforms,target_transform=y_transforms)
     dataloaders = DataLoader(liver_dataset, batch_size=1)
+    model.eval()
     import matplotlib.pyplot as plt
     plt.ion()
     with torch.no_grad():
@@ -70,5 +75,13 @@ def infer():
 
 
 if __name__ == '__main__':
-    train()
-    #infer()
+    parse = argparse.ArgumentParser()
+    parse.add_argument("action", type=str, help="train or test")
+    parse.add_argument("--batch_size", type=int, default=8)
+    parse.add_argument("--ckp", type=str, help="the path of model weight file")
+    args = parse.parse_args()
+
+    if args.action=="train":
+        train()
+    elif args.action=="test":
+        test()
